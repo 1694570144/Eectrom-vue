5043239093eb202ca1909e9e8956a365339dfcd1
jixu <1694570144@qq.com>
2022-12-18 13:37:21 +0800

    init

---
+++ electron/main.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const NODE_ENV = process.env.NODE_ENV
+
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 800, height: 600, webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+  win.loadURL('http://127.0.0.1:5173/')
+
+  if (NODE_ENV === 'development') {
+    win.webContents.openDevTools()
+  }
+
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
